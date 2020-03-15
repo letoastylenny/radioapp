5480e2c57381c39e2fa4c3e498b9c53c0219b8c3
letoastylenny <letoastylenny@protonmail.com>
2020-03-14 19:50:42 -0500

    Added new song class file for storing and using song objects Added ability to queue to next song in song list
    TODO: Songs need to start playing the second they are queued, the next song icon could look nicer, add functionality to get song list from database rather than hardcoded in

---
+++ lib/songClass.dart
@@ -0,0 +1,66 @@
+
+class Song{
+  //Fields
+  String title, artist, infolink, mp3link, coverart, genre;
+
+  //Getters and Setters
+  set songTitle(String title){
+    this.title = title;
+  }
+
+  String get songTitle {
+    return title;
+  }
+
+  set songArtist(String artist){
+    this.artist = artist;
+  }
+
+  String get songArtist {
+    return artist;
+  }
+
+  set songInfo(String infolink){
+    this.infolink = infolink;
+  }
+
+  String get songInfo {
+    return infolink;
+  }
+
+  set songMP3(String mp3link){
+    this.mp3link = mp3link;
+  }
+
+  String get songMP3 {
+    return mp3link;
+  }
+
+  set songArt(String coverart){
+    this.coverart = coverart;
+  }
+
+  String get songArt {
+   return coverart;
+  }
+
+  set songGenre(String genre){
+    this.genre = genre;
+  }
+
+  String get songGenre {
+    return genre;
+  }
+
+  //Constructor
+  Song() {
+    //default nothing
+  }
+
+  Song.constructor(this.title, this.artist, this.infolink, this.mp3link, this.coverart, this.genre);
+
+  //Functions
+  void songData() {
+    print(title + artist + infolink + mp3link + coverart + genre);
+  }
+}
